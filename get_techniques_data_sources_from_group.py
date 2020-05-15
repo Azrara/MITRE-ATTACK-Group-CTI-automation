65f9893f7795be8f8bdfd17544de60d2da557083
Azrara <azrara.9516@gmail.com>
2020-05-15 02:35:03 +0200

    Update get_techniques_data_sources_from_group.py

--- get_techniques_data_sources_from_group.py
+++ get_techniques_data_sources_from_group.py
@@ -3,17 +3,20 @@ from taxii2client import Collection
 import argparse
 import csv
 
+#Connect to TAXII server
 collection = Collection("https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/")
 fs = TAXIICollectionSource(collection)
 
 techniques_data_sources = {}
 
+#Get a group by its name
 def get_group_by_alias(src, alias):
     return src.query([
         Filter('type', '=', 'intrusion-set'),
         Filter('aliases', '=', alias)
     ])
 
+#Return group's techniques
 def get_technique_by_group(src, stix_id):
     relations = src.relationships(stix_id, 'uses', source_only=True)
     return src.query([
@@ -21,6 +24,7 @@ def get_technique_by_group(src, stix_id):
         Filter('id', 'in', [r.target_ref for r in relations])
     ])
 
+#Return all known threat actors
 def get_all_groups(src):
     groups = []
     q = src.query([Filter('type', '=', 'intrusion-set')])
@@ -40,10 +44,11 @@ if group not in get_all_groups(fs):
 group = get_group_by_alias(fs, group)[0]
 techniques = get_technique_by_group(fs, group)
 
-
+#For each technique, append its required data sources
 for item in techniques:
     techniques_data_sources.update({item.name:item.x_mitre_data_sources})
 
+#Store result in a CSV file
 with open("techniques_datasource.csv", "w", newline="") as fd:
     wr = csv.writer(fd)
     wr.writerow(['technique','data source'])
